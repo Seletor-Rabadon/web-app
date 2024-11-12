c3d5fca58f693d37c60a985f0ebf097a4b699b4a
Roger <roger.barreto58@gmail.com>
2024-11-12 09:26:39 -0300

    fix: type error

--- src/services/champion.ts
+++ src/services/champion.ts
@@ -2,7 +2,7 @@ import championData from '../mock/championFull.json';
 import { Champion, ChampionFull } from '@/types/championFull';
 
 export async function getChampionData(championId: string) {
-  const champions = (championData as ChampionFull).data;
+  const champions = (championData as unknown as ChampionFull).data;
 
   return Promise.resolve(
     Object.values(champions).find((champion) => champion.id == championId)
